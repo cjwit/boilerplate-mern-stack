f4ea60edb253afe9b4382e93edd3f24a670b289c
cjwit <chris.witulski@gmail.com>
2021-07-03 15:06:50 -0400

    add local polls listing in database

--- server/database.js
+++ server/database.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient, Timestamp } = require('mongodb');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 
 // List your collection names here
@@ -9,24 +9,26 @@ class databaseManager {
         this.db = null;
         this.server = new MongoMemoryServer();
         this.connection = null;
+        this.polls = null;  // example collection from the sample app
     }
 
     // use local instance of a database for testing purposes
     async startTest() {
         const url = await this.server.getUri();
-        this.connection = await MongoClient.connect(url, { useNewUrlParser: true });
+        this.connection = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true  });
         this.db = this.connection.db(await this.server.getDbName());
+        this.polls = this.db.collection('polls');
+        console.log('Connected to local test database');
     }
 
     // use remote database
     async start() {
         const url = process.env.DBURL;
         this.connection = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-            .then(connectedClient => {
-                this.db = connectedClient.db('boilerplace-mern');
-                this.polls = this.db.collection('polls');
-                console.log('Connected to database')
-            }).catch(err => { console.log(err) });
+        this.db = this.connection.db('boilerplate-mern');
+        this.polls = this.db.collection('polls');
+        console.log('Connected to database');
+
     }
 
     stop() {
