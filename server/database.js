884e8c6d0253cd529772ae34c15c4e55ed9e22fc
cjwit <chris.witulski@gmail.com>
2021-07-03 14:48:40 -0400

    connected to MongoDB

--- server/database.js
+++ server/database.js
@@ -4,17 +4,29 @@ const { MongoMemoryServer } = require('mongodb-memory-server');
 // List your collection names here
 const COLLECTIONS = [];
 
-class DBManager {
+class databaseManager {
     constructor() {
         this.db = null;
         this.server = new MongoMemoryServer();
         this.connection = null;
     }
 
-    async start() {
+    // use local instance of a database for testing purposes
+    async startTest() {
         const url = await this.server.getUri();
         this.connection = await MongoClient.connect(url, { useNewUrlParser: true });
         this.db = this.connection.db(await this.server.getDbName());
+    }
+
+    // use remote database
+    async start() {
+        const url = process.env.DBURL;
+        this.connection = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+            .then(connectedClient => {
+                this.db = connectedClient.db('boilerplace-mern');
+                this.polls = this.db.collection('polls');
+                console.log('Connected to database')
+            }).catch(err => { console.log(err) });
     }
 
     stop() {
@@ -27,4 +39,4 @@ class DBManager {
     }
 }
 
-module.exports = DBManager;
+module.exports = databaseManager;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 // determine environment and load appropriate variables
 NODE_ENV = process.env.NODE_ENV || 'development'
 if (process.env.NODE_ENV !== 'production') {
-    console.log('In a development environment, loading variables.');
+    console.log('In a development environment, loading variables');
     require('dotenv').config();
 }
 
-const dbManager = require('./dbmanager');
+const databaseManager = require('./database');
 const express = require('express');
 const path = require('path');
 const compress = require('compression');
@@ -23,8 +23,12 @@ app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, '../app/dist/index.html'));
 })
 
-// listen
-const port = process.env.PORT || 8080
-app.listen(port, function() {
-    console.log(`Listening on port ${port}...`);
-})
+// connect database and listen
+dbManager = new databaseManager;
+dbManager.start()
+    .then(() => {
+        const port = process.env.PORT || 8080
+        app.listen(port, function() {
+            console.log(`Listening on port ${port}...`);
+        })
+    })
